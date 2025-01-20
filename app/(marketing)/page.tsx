e28719febdc515a848bdfb3bba1648fab414bf07
Aleksej Dix <git@aleksejdix.com>
2025-01-20 19:59:38 +0100

    add new blocks

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,26 +1,34 @@
 // import { MoveRight, PhoneCall } from "lucide-react"
 
+import { Faqs } from "@/components/blocks/Faqs"
+import { Services } from "@/components/blocks/Services"
+
 // import { NewsletterFormSection } from "@/components/emails/newletter-form-banner"
 
 // import { Button } f rom "@/components/ui/button"
 
 const Page = () => (
-  <main className="container max-w-2xl md:py-20 lg:py-40">
-    <div>
-      {/* <Button variant="secondary" size="sm" className="gap-4">
+  <main className=" md:py-20 lg:py-40">
+    <div className="container max-w-2xl">
+      <div>
+        {/* <Button variant="secondary" size="sm" className="gap-4">
             Read our launch article <MoveRight className="w-4 h-4" />
-          </Button> */}
+            </Button> */}
+      </div>
+      <div className="space-y-4 md:text-center">
+        <h1 className="text-4xl font-bold md:text-7xl max-w-2xl tracking-tighter text-pretty">
+          Master Web Accessibility with Allyship
+        </h1>
+        <p className="text-pretty text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-xl md:mx-auto">
+          Learn web accessibility through interactive guides, videos, and
+          articles. Allyship gives you the tools to create inclusive,
+          user-friendly websites effortlessly. Let’s build the best site
+          together!
+        </p>
+      </div>
     </div>
-    <div className="space-y-4 md:text-center">
-      <h1 className="text-4xl font-bold md:text-7xl max-w-2xl tracking-tighter text-pretty">
-        Master Web Accessibility with Allyship
-      </h1>
-      <p className="text-pretty text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-xl md:mx-auto">
-        Learn web accessibility through interactive guides, videos, and
-        articles. Allyship gives you the tools to create inclusive,
-        user-friendly websites effortlessly. Let’s build the best site together!
-      </p>
-    </div>
+    <Services />
+    <Faqs />
     {/* <NewsletterFormSection /> */}
     {/* <div className="flex flex-row gap-3">
           <Button size="lg" className="gap-4" variant="outline">
---
+++ components/blocks/Services.tsx
@@ -0,0 +1,86 @@
+import {
+  Accessibility,
+  BadgeCheck,
+  Code,
+  Eye,
+  PersonStanding,
+  Scale,
+} from "lucide-react"
+
+const services = [
+  {
+    name: "E2E Accessibility Testing",
+    description:
+      "Comprehensive end-to-end tests to ensure your web apps are accessible to all users.",
+    icon: Accessibility,
+  },
+  {
+    name: "Accessibility Audits",
+    description:
+      "In-depth audits to identify accessibility issues and provide actionable recommendations.",
+    icon: Eye,
+  },
+  {
+    name: "Accessibility Compliance",
+    description:
+      "Ensure your applications meet WCAG and ADA compliance standards.",
+    icon: Scale,
+  },
+  {
+    name: "Accessibility Training",
+    description:
+      "Empower your team with the knowledge to build and maintain accessible applications.",
+    icon: PersonStanding,
+  },
+  {
+    name: "Automated E2E Testing",
+    description:
+      "Automated testing to catch accessibility issues early in development.",
+    icon: BadgeCheck,
+  },
+  {
+    name: "Vue.js Frontend Consulting",
+    description:
+      "Expert guidance to optimize your frontend for performance, accessibility, and scalability.",
+    icon: Code,
+  },
+]
+
+export function Services() {
+  return (
+    <section className="py-28">
+      <div className="max-w-4xl mx-auto px-8">
+        <header className="max-w-xl mx-auto lg:text-center">
+          <h2 className="text-2xl font-bold md:text-3xl">
+            Services to Build Inclusive Digital Experiences
+          </h2>
+          <p className="text-foreground/60 mt-6 md:text-lg">
+            From training your team to ensuring compliance, we provide the tools
+            and expertise to create accessible and user-friendly applications.
+          </p>
+        </header>
+
+        <div className="max-w-2xl mx-auto mt-16 md:mt-20 lg:mt-24 lg:max-w-4xl">
+          <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-2 lg:gap-y-16">
+            {services.map((service) => (
+              <div key={service.name} className="relative pl-16">
+                <div className="font-semibold">
+                  <div className="absolute left-0 top-0 flex w-9 h-9 items-center justify-center rounded-lg border-[1.5px] border-foreground">
+                    <service.icon
+                      aria-hidden="true"
+                      className="h-5 w-5 stroke-[1.5px]"
+                    />
+                  </div>
+                  {service.name}
+                </div>
+                <div className="text-foreground/60 mt-2">
+                  {service.description}
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
