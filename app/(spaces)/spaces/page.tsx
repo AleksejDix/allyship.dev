05c50fb1c811af10ee3a5cf153f05c44c245317a
Aleksej Dix <git@aleksejdix.com>
2025-02-09 06:12:55 +0100

    change spaces

--- app/(spaces)/spaces/page.tsx
+++ app/(spaces)/spaces/page.tsx
@@ -1,39 +1,17 @@
 import { Suspense } from "react"
-import Link from "next/link"
 import { getSpaces } from "@/features/space/actions"
 import { SpaceCreate } from "@/features/space/components/space-create"
+import { SpaceIndex } from "@/features/space/components/space-index"
 
-import { Card, CardHeader } from "@/components/ui/card"
-
-// Create a separate component for the spaces list
-async function SpacesList() {
+export default async function Page() {
   const { spaces } = await getSpaces()
 
-  return (
-    <div className="grid gap-4 mt-8">
-      {spaces.map((space) => (
-        <Card key={space.id} className="transition-colors hover:bg-accent">
-          <CardHeader>
-            <Link
-              href={`/${space.id}`}
-              className="text-lg font-medium transition-colors hover:text-muted-foreground"
-            >
-              {space.name}
-            </Link>
-          </CardHeader>
-        </Card>
-      ))}
-    </div>
-  )
-}
-
-export default function SpacePage() {
   return (
     <>
       <div className="container mx-auto py-12">
         <Suspense fallback={<div>Loading spaces...</div>}>
           <SpaceCreate />
-          <SpacesList />
+          <SpaceIndex spaces={spaces} />
         </Suspense>
       </div>
     </>
