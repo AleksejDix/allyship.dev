0d0f6d93c1cc892eef503484dad62a110acab315
Aleksej Dix <git@aleksejdix.com>
2025-02-09 13:39:55 +0100

    improve glossary

--- app/(marketing)/glossary/page.tsx
+++ app/(marketing)/glossary/page.tsx
@@ -1,12 +1,13 @@
 import { notFound } from "next/navigation"
+import { GlossarySearch } from "@/features/glossary/components/glossary-search"
 import { allTerms } from "contentlayer/generated"
-import { PageHeader } from "@/components/page-header"
+
 import { Separator } from "@/components/ui/separator"
-// import { Mdx } from "@/components/mdx-components"
+import { PageHeader } from "@/components/page-header"
 
 import "@/styles/mdx.css"
 
-export default async function PostPage() {
+export default async function GlossaryPage() {
   const terms = allTerms
 
   if (!terms) {
@@ -16,24 +17,12 @@ export default async function PostPage() {
   return (
     <div className="container py-8">
       <div>
-        <PageHeader heading="Glossary" description="Learn the language of web accessibility." />
+        <PageHeader
+          heading="Glossary"
+          description="Learn the language of web accessibility and development."
+        />
         <Separator className="my-8" />
-
-        <ul className="space-y-2 max-w-[65ch]">
-          {terms.map((term) => {
-            return (
-              <li
-                key={term.slug}
-                className="border-accent border p-4 rounded-lg"
-              >
-                <h2 className="lg font-bold text-foreground">{term.title}</h2>
-                <div className="text-sm text-muted-foreground">
-                  {term.description}
-                </div>
-              </li>
-            )
-          })}
-        </ul>
+        <GlossarySearch terms={terms} />
       </div>
     </div>
   )
