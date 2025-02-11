14700d9ecc800db6f7ed7cd7020670442faee81f
Aleksej Dix <git@aleksejdix.com>
2025-02-11 10:17:36 +0100

    fix add space dialog

--- app/(spaces)/spaces/page.tsx
+++ app/(spaces)/spaces/page.tsx
@@ -1,18 +1,7 @@
-import { Suspense } from "react"
 import { PageHeader } from "@/features/domain/components/page-header"
 import { getSpaces } from "@/features/space/actions"
-import { SpaceCreate } from "@/features/space/components/space-create"
+import { CreateSpaceDialog } from "@/features/space/components/create-space-dialog"
 import { SpaceIndex } from "@/features/space/components/space-index"
-
-import { Button } from "@/components/ui/button"
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "@/components/ui/dialog"
 
 export default async function Page() {
   const { spaces } = await getSpaces()
@@ -26,23 +15,7 @@ export default async function Page() {
         title="Spaces"
         description="Manage your spaces and their accessibility settings."
       >
-        <Dialog>
-          <DialogTrigger asChild>
-            <Button>Create Space</Button>
-          </DialogTrigger>
-          <DialogContent>
-            <DialogHeader>
-              <DialogTitle>Create Space</DialogTitle>
-              <DialogDescription>
-                Create a new space to manage your websites and track their
-                accessibility.
-              </DialogDescription>
-            </DialogHeader>
-            <Suspense fallback={<div>Loading...</div>}>
-              <SpaceCreate />
-            </Suspense>
-          </DialogContent>
-        </Dialog>
+        <CreateSpaceDialog />
       </PageHeader>
 
       <div className="container">
