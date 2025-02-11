03086bf01165aa1a11e5687d17166ad9a4794212
Aleksej Dix <git@aleksejdix.com>
2025-02-11 09:32:01 +0100

    get correct types from the database

--- app/(marketing)/scans/[...id]/page.tsx
+++ app/(marketing)/scans/[...id]/page.tsx
@@ -1,5 +1,6 @@
 // import { getScan } from "@/features/scans/actions"
 // import { TestEngine } from "@/features/scans/components/test-engine"
+import { notFound } from "next/navigation"
 import { ScanShow } from "@/features/scans/components/scan-show"
 
 import { createClient } from "@/lib/supabase/server"
@@ -10,6 +11,10 @@ export default async function ScanPage({ params }: { params: { id: string } }) {
   const [id] = await params.id
   const supabase = await createClient()
   const { data } = await supabase.from("Scan").select().match({ id }).single()
+
+  if (!data) {
+    return notFound()
+  }
 
   return <ScanShow serverProps={data} />
 }
