16bc5647defbeeceee09022f1a1c17b5779049b9
Aleksej Dix <git@aleksejdix.com>
2025-02-13 15:29:20 +0100

    migrate away from contentlayer because it is not updated

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,9 +1,7 @@
 import type { MDXComponents } from "mdx/types";
-import { Code } from "./components/code";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     ...components,
-    Code,
   };
 }
