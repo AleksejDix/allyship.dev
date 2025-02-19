09926a63944d17b42b56555545ff7f5aaf919f55
Aleksej Dix <git@aleksejdix.com>
2025-02-19 18:04:18 +0100

    add nice sidebar

---
+++ apps/allystudio/sidepanel.tsx
@@ -0,0 +1,10 @@
+export default function SidePanel() {
+  return (
+    <div className="flex h-screen w-full flex-col p-4">
+      <h1 className="text-xl font-bold">Allyship Studio</h1>
+      <div className="mt-4">
+        <p>Welcome to Allyship Studio!</p>
+      </div>
+    </div>
+  )
+}
