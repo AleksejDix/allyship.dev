d22aa2aae324d331b87626f389087dae11db1dc5
Aleksej Dix <git@aleksejdix.com>
2025-02-21 12:51:33 +0100

    add user to the options page

---
+++ apps/allystudio/src/components/options-header.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import { supabase } from "@/core/supabase"
+import { type Session } from "@supabase/supabase-js"
+import logo from "data-base64:~assets/logo.png"
+import * as Icons from "lucide-react"
+
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Button } from "./ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from "./ui/dropdown-menu"
+
+interface OptionsHeaderProps {
+  session: Session | null
+}
+
+export function OptionsHeader({ session }: OptionsHeaderProps) {
+  const userInitials = session?.user.email
+    ?.split("@")[0]
+    .slice(0, 2)
+    .toUpperCase()
+
+  return (
+    <header className="border-b">
+      <div className="container flex h-14 items-center justify-between px-4">
+        <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 rounded-full bg-black p-1">
+            <img src={logo} alt="ALLYSTUDIO" width={32} height={32} />
+          </div>
+          <h1 className="text-xl font-bold">ALLYSTUDIO</h1>
+        </div>
+
+        {session && (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <span className="sr-only">
+                  {session.user.user_metadata.name || session.user.email}
+                </span>
+                <Avatar className="h-8 w-8">
+                  <AvatarImage
+                    src={session.user.user_metadata.avatar_url}
+                    alt={session.user.email || ""}
+                  />
+                  <AvatarFallback>{userInitials}</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+              <DropdownMenuLabel className="font-normal">
+                <div className="flex flex-col space-y-1">
+                  <p className="text-sm font-medium leading-none">
+                    {session.user.email}
+                  </p>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => supabase.auth.signOut()}
+                className="text-red-600 dark:text-red-400">
+                <Icons.LogOut className="mr-2 h-4 w-4" />
+                <span>Log out</span>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
+      </div>
+    </header>
+  )
+}
