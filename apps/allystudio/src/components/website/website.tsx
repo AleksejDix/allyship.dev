372b94c31426bb8f150b1e33a316d522356ea3b1
Aleksej Dix <git@aleksejdix.com>
2025-02-26 00:10:23 +0100

    website selection

--- apps/allystudio/src/components/website/website.tsx
+++ apps/allystudio/src/components/website/website.tsx
@@ -1,9 +1,9 @@
 import { useSelector } from "@xstate/react"
-import { memo } from "react"
+import { memo, useEffect, useRef } from "react"
 import type { PropsWithChildren } from "react"
 
 import { useSpaceContext } from "../space/space-context"
-import { WebsiteProvider } from "./website-context"
+import { useWebsiteContext, WebsiteProvider } from "./website-context"
 import { WebsiteDebug } from "./website-debug"
 import { WebsiteEmpty } from "./website-empty"
 import { WebsiteOptions } from "./website-options"
@@ -31,13 +31,41 @@ const Website = memo(function Website({
 
   return (
     <WebsiteProvider spaceId={currentSpace.id}>
-      <WebsiteEmpty />
-      <WebsiteOptions />
-      <WebsiteSelected>{children}</WebsiteSelected>
+      <WebsiteContent spaceId={currentSpace.id}>{children}</WebsiteContent>
       {debug && <WebsiteDebug />}
     </WebsiteProvider>
   )
 })
+
+// Separate component to handle space changes
+function WebsiteContent({
+  children,
+  spaceId
+}: PropsWithChildren<{ spaceId: string }>) {
+  const websiteActor = useWebsiteContext()
+  const previousSpaceIdRef = useRef<string>(spaceId)
+
+  // Send SPACE_CHANGED event when spaceId changes
+  useEffect(() => {
+    // Only send the event if the space ID has actually changed
+    if (previousSpaceIdRef.current !== spaceId) {
+      console.log(
+        "Space changed, sending SPACE_CHANGED event with ID:",
+        spaceId
+      )
+      websiteActor.send({ type: "SPACE_CHANGED", spaceId })
+      previousSpaceIdRef.current = spaceId
+    }
+  }, [spaceId, websiteActor])
+
+  return (
+    <>
+      <WebsiteEmpty />
+      <WebsiteOptions />
+      <WebsiteSelected>{children}</WebsiteSelected>
+    </>
+  )
+}
 
 // Named exports for composite components
 export { Website, WebsiteEmpty, WebsiteOptions, WebsiteDebug }
